dd1f5f8645f95010fdb0c7f3aa2d31dd317ae90e
Sanjoy Debnath <s4sanjoy@yahoo.com>
2018-01-01 14:08:44 +0600

    First commit

---
+++ PCERP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PCERP.Startup))]
+namespace PCERP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
